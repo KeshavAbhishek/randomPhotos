d59b29de61d20f1b44f7bb1a097c48d2ab7e6563
KeshavAbhishek <crystaled2003@gmail.com>
2022-03-21 11:18:08 +0530

    Editing Done

--- node.js
+++ node.js
@@ -2,7 +2,7 @@ function setImageFn(){
     var widthToBe = parseInt(window.innerWidth*(parseInt(document.getElementById('widthInputEntry').value))/100);
     var heightToBe = parseInt(window.innerHeight*(parseInt(document.getElementById('heightInputEntry').value))/100);
 
-    document.getElementById('mainFrame').style.background=`url(https://picsum.photos/${widthToBe}/${heightToBe})`;
+    document.getElementById('mainFrame').style.backgroundImage=`url(https://picsum.photos/${widthToBe}/${heightToBe})`;
 
     document.getElementById('aboveContainer').style.display='flex';
 
